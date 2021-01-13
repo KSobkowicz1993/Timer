afea2e376ac16cd7850481761726a0866f61dea5
Karolina Sobkowicz <karolina.klapyta@gmail.com>
2021-01-13 18:45:45 +0100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//zegarek
+setInterval(() => {
+    const date = new Date;
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    if (hour < 10) {
+        hour = `0${hour}`;
+    } 
+
+    if (min < 10) {
+        min = `0${min}`;
+    }
+
+    if (sec < 10) {
+        sec = `0${sec}`;
+    }
+
+    document.querySelector('#timer').innerText = `${hour}:${min}:${sec}`;
+
+}, 1000);
